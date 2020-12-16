0546c43aeb9087853919f435519d394a6be7ef65
Lakshay saini <lakshaysaini2013@gmail.com>
2020-12-16 15:21:11 +0530

    16122020

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -1,29 +1,72 @@
 import React from "react";
-import defaultData from "./data";
 
 class App extends React.Component {
-  state = { data: null };
+  state = { data: null, convert: null, amount: "", result: 0 };
   handleSelect = (e) => {
     if (e.target.value !== "") {
-      this.setState({ data: defaultData[e.target.value] });
+      fetch(`https://api.exchangeratesapi.io/latest?base=${e.target.value}`)
+        .then((res) => res.json())
+        .then((res) =>
+          this.setState((prevState) => {
+            return { ...prevState, data: res.rates };
+          })
+        );
     } else {
-      this.setState({ data: null });
+      this.setState((prevState) => {
+        return { ...prevState, data: null };
+      });
     }
   };
+  handleSelect2 = (e) => {
+    this.setState((prevState) => {
+      return { ...prevState, convert: e.target.value };
+    });
+  };
+  handleInput = (e) => {
+    this.setState((prevState) => {
+      return { ...prevState, amount: e.target.value };
+    });
+  };
+
+  onSubmitChange = (e) => {
+    e.preventDefault();
+    let converted =
+      parseInt(this.state.data[this.state.convert]) *
+      parseInt(this.state.amount);
+    this.setState((prevState) => {
+      return { ...prevState, result: converted };
+    });
+    console.log(parseInt("10"));
+  };
+
   render() {
     return (
       <div className="App">
-        <form>
+        <form onSubmit={this.onSubmitChange}>
           <select onChange={this.handleSelect}>
-            <option value="" selected>
+            <option value="" defaultValue>
               Select Option
             </option>
-            <option value="psg"> Psg</option>
-            <option value="up"> UP</option>
-            <option value="uttarakhand"> Uttarakhand</option>
+            <option value="USD"> USD</option>
+            <option value="INR"> IND</option>
+            <option value="EUR"> EUR</option>
           </select>
+          <select onChange={this.handleSelect2}>
+            <option value="" defaultValue>
+              Select Option
+            </option>
+            <option value="USD"> USD</option>
+            <option value="INR"> IND</option>
+            <option value="EUR"> EUR</option>
+          </select>
+          <input
+            type="text"
+            value={this.state.amount}
+            onChange={this.handleInput}
+          />
+          <input type="submit" />
         </form>
-        {this.state.data && this.state.data.map((item) => <p>{item.title}</p>)}
+        {this.state.result}
       </div>
     );
   }
