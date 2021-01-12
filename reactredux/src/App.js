73d10fe5e4f4ba5902a17d4527a1a1ec5a258bbd
Lakshay saini <lakshaysaini2013@gmail.com>
2021-01-12 23:49:35 +0530

    fb login

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -1,39 +1,20 @@
 import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
 
-class App extends Component {
-  state = { currentDate: null, imgUrl: null };
-
-  handelchange = (e) => {
-    e.preventDefault();
-    const api = async (data) => {
-      const img = await fetch(
-        `https://api.nasa.gov/planetary/apod?api_key=ri0EZkle2DPanZWtyS8oZFgC4coub86CDtV5OiYV&date=${data}`
-      );
-      const { url } = await img.json();
-      this.setState((prev) => ({ ...prev, imgUrl: url }));
-    };
-    api(e.target.value);
-    this.setState((prev) => ({ ...prev, currentDate: e.target.value }));
+export default class App extends Component {
+  responseFacebook = (response) => {
+    console.log(response);
   };
-
   render() {
     return (
-      <>
-        <form>
-          {/* <select onChange={this.handleSelect}>
-            <option value="" defaultValue>
-              Select Option
-            </option>
-            <option value="USD"> USD</option>
-            <option value="INR"> IND</option>
-            <option value="EUR"> EUR</option>
-          </select> */}
-          <input type="date" onChange={this.handelchange} />
-        </form>
-        {this.state.imgUrl && <img src={`${this.state.imgUrl}`} alt="img" />}
-      </>
+      <FacebookLogin
+        appId="191045292645821"
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={this.responseFacebook}
+        cssClass="my-facebook-button-class"
+        icon="fa-facebook"
+      />
     );
   }
 }
-
-export default App;
